42f97bce1c0c278effa9dfa9a77e8019132490c9
Soesh Cooray <soeshcooray@gmail.com>
2026-06-22 00:40:52 +0530

    change contentful api key

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 //Contentful's Content Delivery API (CDA) token (which you are using) is read-only and designed to be public. It is safe to expose in client-side code because it only allows reading published content.
 const CONFIG = {
     CONTENTFUL: {
-        SPACE_ID: 'cr2v5nbc0rx2',
-        ACCESS_TOKEN: 'qmEA1IrWGYo8EfA_C8tK9wZqWUS8kLWt4QymemY1O4k',
+        SPACE_ID: 'p5l7mdqwr9is',
+        ACCESS_TOKEN: 'emQmjLozvwHRbcxcpC2nGOmEBLfueErm_aN2Wo-yafU',
         CONTENT_TYPE: 'news'
     }
 };
